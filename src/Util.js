d27242cca4c2e29ed170f76c4a5f4d3c07fc64b0
David Thevenin <david.thevenin@gmail.com>
2013-06-21 10:36:53 +0900

    Use a deep clone for the array, instead of slice.

--- src/Util.js
+++ src/Util.js
@@ -1591,7 +1591,7 @@ Array.prototype.removeAll = function ()
  */
 Array.prototype.clone = function ()
 {
-  return this.slice ();
+  return clone (this);
 };
 
 /********************************************************************
