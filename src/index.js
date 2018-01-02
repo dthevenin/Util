3d984542c0c4337203e742fcd682439b463e2597
David Thevenin <david.thevenin@gmail.com>
2018-01-02 23:34:29 +0900

    fix missing export

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
-export { clone } from './utils/helpers';
-export * from './utils/is';
 export { } from './utils/Array';
+import { clone, toJSON } from './utils/helpers';
 import Point from './utils/Point';
 import CSSMatrix from './utils/CSSMatrix';
-export { Point, CSSMatrix };
+
+export {
+  clone,
+  toJSON,
+  Point,
+  CSSMatrix
+};
+export * from './utils/String';
 export * from './utils/Utils';
+export * from './utils/is';
