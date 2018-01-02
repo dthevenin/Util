fdfe0ce560b1ca2f4522d07ebf59999a28ce4a47
David Thevenin <david.thevenin@gmail.com>
2018-01-02 23:34:42 +0900

    Update libs

--- es/vs_utils.js
+++ es/vs_utils.js
@@ -113,6 +113,18 @@ function clone (object)
 }
 
 /**
+ *  Returns a JSON string.
+ *
+ *  @memberOf vs.util
+ *
+ * @param {Object} value The object to be serialized.
+ **/
+function toJSON (value)
+{
+  return JSON.stringify (value);
+}
+
+/**
   Copyright (C) 2009-2012. David Thevenin, ViniSketch SARL (c), and
   contributors. All rights reserved
 
@@ -1167,6 +1179,161 @@ class Point {
 
     return result;
   }
+}
+
+/**
+  Copyright (C) 2009-2012. David Thevenin, ViniSketch SARL (c), and
+  contributors. All rights reserved
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Lesser General Public License as published
+  by the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License
+  along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/********************************************************************
+                    String manipulation
+*********************************************************************/
+
+const __date_reg_exp = /\/Date\((-?\d+)\)\//;
+
+/**
+ * HTML-encodes a string and returns the encoded string.
+ *
+ *  @memberOf vs.util
+ *
+ * @param {String} str String The string
+ */
+function htmlEncode (str)
+{
+  if (!isString (str)) return '';
+
+  return str.replace (/&/g, "&amp;").
+    replace(/</g, "&lt;").replace(/>/g, "&gt;");
+}
+
+/**
+ *  Strips all leading and trailing whitespace from a string.
+ *
+ *  @memberOf vs.util
+ *
+ * @param {String} str String The string
+ */
+function strip$1 (str)
+{
+  if (!isString (str)) return '';
+
+  return str.replace(/^\s+/, '').replace(/\s+$/, '');
+}
+
+/**
+ *  Converts a string separated by dashes into a camelCase equivalent
+ *
+ *  @memberOf vs.util
+ *
+ * @param {String} str String The string
+ * @return {String} the result
+ */
+function camelize$1 (str)
+{
+  if (!isString (str)) return '';
+
+  var parts = str.split ('-'), len = parts.length;
+  if (len === 1) { return parts [0]; }
+
+  var camelized = str.charAt (0) === '-'
+    ? parts [0].charAt (0).toUpperCase () + parts [0].substring (1)
+    : parts [0];
+
+  for (var i = 1; i < len; i++)
+    camelized += parts[i].charAt (0).toUpperCase() + parts[i].substring (1);
+
+  return camelized;
+}
+
+/**
+ *  Converts a string separated by dashes into a camelCase equivalent
+ *
+ *  @memberOf vs.util
+ *
+ * @param {String} str String The string
+ * @return {String} the result
+ */
+function capitalize (str)
+{
+  if (!isString (str)) return '';
+
+  return str.charAt(0).toUpperCase() + str.substring(1).toLowerCase();
+}
+
+/**
+ *  Converts a camelized string into a series of words separated by an
+ *  underscore (_).
+ *
+ *  @memberOf vs.util
+ *
+ * @param {String} str String The string
+ * @return {String} the result
+ */
+function underscore (str)
+{
+  if (!isString (str)) return '';
+
+  return str.replace (/::/g, '/')
+            .replace (/([A-Z]+)([A-Z][a-z])/g, '$1_$2')
+            .replace (/([a-z\d])([A-Z])/g, '$1_$2')
+            .replace (/-/g, '_')
+            .toLowerCase ();
+}
+
+/**
+ *  Parse a json string. <p/>
+ *  This function use the JSON.parse function but it manage also
+ *  Date parsing wich is not managed by the JSON.parse
+ *
+ *  @memberOf vs.util
+ *  @ignore
+ *
+ * @param {String} str String The string
+ * @return {Object} the result
+ */
+function parseJSON (json)
+{
+  if (!json) return null;
+  var temp = JSON.parse (json);
+
+  if (!__date_reg_exp.test (json)) return temp;
+
+  function manageDate (obj)
+  {
+    if (isString (obj))
+    {
+      var result = __date_reg_exp.exec (obj);
+      if (result && result [1]) // JSON Date -> Date generation
+      {
+        obj = new Date (parseInt (result [1]));
+      }
+    }
+    else if (isArray (obj))
+    {
+      for (var i = 0; i < obj.length; i++) { obj [i] = manageDate (obj [i]); }
+    }
+    else if (obj instanceof Date) { return obj; }
+    else if (obj instanceof Object)
+    {
+      for (var key in obj) { obj [key] = manageDate (obj [key]); }
+    }
+    return obj;
+  }
+  return manageDate (temp);
 }
 
 /**
@@ -2592,4 +2759,4 @@ const getBoundingClientRect =
   (vsTestElem && vsTestElem.getBoundingClientRect) ?
   _getBoundingClientRect_api2: _getBoundingClientRect_api1;
 
-export { Point, CSSMatrix, clone, isElement, isArray, isFunction, isString, isNumber, isObject, isUndefined, vsTestElem, vsTestStyle, clearImmediate, setImmediate, requestAnimationFrame, cancelRequestAnimationFrame, CSS_VENDOR, SUPPORT_3D_TRANSFORM, SUPPORT_CSS_TRANSFORM, extendClass, defineProperty, defineClassProperty, defineClassProperties, free, extend, hasClassName, addClassName, removeClassName, toggleClassName, addCssRule, addCssRules, getElementHeight, getElementWidth, getElementDimensions, getElementStyle, setElementStyle, setElementOpacity, getElementOpacity, getElementAbsolutePosition, setElementPos, setElementSize, setElementVisibility, isElementVisible, removeAllElementChild, safeInnerHTML, setElementInnerText, setElementTransform, getElementTransform, getElementMatrixTransform, setElementTransformOrigin, getBoundingClientRect, importFile };
+export { clone, toJSON, Point, CSSMatrix, htmlEncode, strip$1 as strip, camelize$1 as camelize, capitalize, underscore, parseJSON, __date_reg_exp, vsTestElem, vsTestStyle, clearImmediate, setImmediate, requestAnimationFrame, cancelRequestAnimationFrame, CSS_VENDOR, SUPPORT_3D_TRANSFORM, SUPPORT_CSS_TRANSFORM, extendClass, defineProperty, defineClassProperty, defineClassProperties, free, extend, hasClassName, addClassName, removeClassName, toggleClassName, addCssRule, addCssRules, getElementHeight, getElementWidth, getElementDimensions, getElementStyle, setElementStyle, setElementOpacity, getElementOpacity, getElementAbsolutePosition, setElementPos, setElementSize, setElementVisibility, isElementVisible, removeAllElementChild, safeInnerHTML, setElementInnerText, setElementTransform, getElementTransform, getElementMatrixTransform, setElementTransformOrigin, getBoundingClientRect, importFile, isElement, isArray, isFunction, isString, isNumber, isObject, isUndefined };
