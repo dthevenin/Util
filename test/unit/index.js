1ef919bbb333f0a2c78a54ff73fa71fd19587e68
David Thevenin <david@thevenin.jp>
2023-01-05 18:36:50 -1000

    Update

---
+++ test/unit/index.js
@@ -0,0 +1,14 @@
+export { } from './utils/Array';
+import { clone, toJSON } from './utils/helpers';
+import Point from './utils/Point';
+import CSSMatrix from './utils/CSSMatrix';
+
+export {
+  clone,
+  toJSON,
+  Point,
+  CSSMatrix
+};
+export * from './utils/String';
+export * from './utils/Utils';
+export * from './utils/is';
